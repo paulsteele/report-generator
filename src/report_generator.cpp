99ea48d992eab702b51b03d5d0187e59ebe86a6e
paulsteele <paul_steele@live.com>
2014-05-08 09:28:58 -0400

    began work on parse_template

--- src/report_generator.cpp
+++ src/report_generator.cpp
@@ -10,13 +10,28 @@ Reads in a LaTeX source file and will replace ~FIELD~ with
 user selected input
 ----------------------------------------------------------*/
 #include "report_generator"
+namespace report_generator {
+	void parse_template(string* file, list<string*>* fills){
+		std::fstream in;
+		in.open(*file, std::fstream::in);
+		if (!in.good()) {
+			cout << *file << " does not exist. Exiting\n";
+		}
+		in.close();
+	}
+}
 
 int main(int argc, char** argv) {
 	Arguments* args = new Arguments(argc, argv);
 	string* file = args->value(string("file"));
+	if (file == NULL){
+		//exit conditions
+		cout << "No file specified. Exiting\n";
+		delete args;
+		return 0;
+	}
+	file->insert(0,"templates/");
+	list<string*>* fills = new list<string*>;
+	report_generator::parse_template(file, fills);
 	delete args;
-}
-
-namespace report_generator {
-
 }
